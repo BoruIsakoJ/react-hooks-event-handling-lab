75a4807f95459695fada912c76f60c3c74f95604
Boru Isako <isco.jaldy@gmail.com>
2025-04-10 14:54:10 +0300

    Completed labs

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function handleFocus(){
+        console.log("Good!")
+    }
+    function handleBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <>
+            <button style={{marginTop:"20px"}} onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <label htmlFor="password">Password: </label>
+            <input id="password" onChange={handleChange} type="password"/>
+        </div>
     )
 }
 
